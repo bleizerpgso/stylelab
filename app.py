00df24f8ac432e4e319ddc02153deb21a70ac5e5
Bleizer <04ocarulla@gmail.com>
2023-04-21 17:24:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,50 +10,77 @@ conn = pymysql.connect(
 print("conexión establecida")
 
 #funcion para filtrar la ropa en mysql
-def buscador(tags):
-    return f"select id, marca, modelo from ropa where tags like '%{tags}%' "
+def buscador(tags, tags2):
+    return f"select id, marca, modelo from ropa where tags like '%{tags}%' or tags like '%{tags2}%'"
+
 
 # Selección de las 3 primeras marcas de ropa
-print("Elije entre estas 3 marcas de ropa:")
-print("H&M (1)")
-print("Nike(2)")
-print("Polo Ralph Lauren(3)")
+
+
+print("""
+SELECIONA ENTRE ESTAS 3 MARCAS ⤵
+
+    +===============================+
+    ||           H&M (1)           ||
+    +===============================+
+    ||           NIKE (2)          ||
+    +===============================+
+    ||           POLO (3)          ||
+    +===============================+
+""")
 primario= int(input("Marca a escoger: "))
-primera_seleccion = {1: "casual", 2:"deporte", 3:"pijo"}
+if primario == 1 or primario == 2 or primario == 3:
+    primera_seleccion = {1: "casual", 2:"deporte", 3:"pijo"}
+else:
+    print("No es un numeor valido.")
 
 print("Ahora te pondremos otras marcas de ropa relacionadas con tu selección:")
 time.sleep(2)
 
 
 if primario == 1:
-    print("Zara(1)")
-    print("Primark(2)")
-    print("Pull and Bear(3)")
+    print("""
+    +===============================+
+    ||           ZARA (1)          ||
+    +===============================+
+    ||          PRIMARK (2)        ||
+    +===============================+
+    ||       PULL AND BEAR (3)     ||
+    +===============================+
+    """)
     secundario = int(input("Ahora de estas 3 marcas selecciona la que mas te guste: "))
     segunda_seleccion = {1: "elegante", 2:"señora", 3:"joven"}
 elif primario == 2:
-    print("Adidas(1)")
-    print("The North Face(2)")
-    print("Jordan(3)")
+    print("""
+
+    +===============================+
+    ||          ADIDAS (1)         ||
+    +===============================+
+    ||      THE NORTH FACE (2)     ||
+    +===============================+
+    ||          JORDAN (3)         ||
+    +===============================+
+
+    """)
     secundario = int(input("Ahora de estas 3 marcas selecciona la que mas te guste: "))
     segunda_seleccion = {1: "futbol", 2:"montaña", 3:"basquet"}
 elif primario == 3:
-    print("Stone Island(1)")
-    print("Loius Vuitton(2)")
-    print("Lacoste(3)")
+    print ("""
+    +===============================+
+    ||        STONE ISLAND (1)     ||
+    +===============================+
+    ||        LOUIS VUITTON (2)    ||
+    +===============================+
+    ||          LACOSTE (3)        ||
+    +===============================+
+    """)
     secundario = int(input("Ahora de estas 3 marcas selecciona la que mas te guste: "))
     segunda_seleccion = {1: "cayetano", 2:"exclusivo", 3:"comun"}
-
-
-
-
-print (segunda_seleccion[secundario])
-print (primera_seleccion[primario])
 
 #Hace la consulta mysql a la funcion
 cursor = conn.cursor()
 cursor.execute(
-    buscador("calle")
+    buscador("calle", "test")
 )
 for id, nombre, modelo in cursor.fetchall():
     print("{0} {1}".format(nombre, modelo))
