471cfe54e94a34bcef1475a618cc0af53452e3d2
Bleizer <04ocarulla@gmail.com>
2023-05-02 15:19:28 +0200

    a

--- sql.py
+++ sql.py
@@ -1,6 +1,25 @@
+import mysql.connector
+
+mysql = mysql.connector.connect(
+    host="localhost", user="root",
+    passwd="6177", db="app"
+)
+
+
+
+print("conexión establecida")
+def buscador(id, marca, modelo, precio, tags):
+    return f"insert into ropa values ('{id}','{marca}','{modelo}', '{precio}' ,'{tags}');"
+
 id = (input("Introduce el id: "))
 marca = (input("Introduce la marca: "))
 modelo = (input("Introduce el modelo: "))
+precio = (input("Introduce el precio: "))
 tags = (input ("Tag de la prenda: "))
 
-print(f"insert into ropa values ('{id}','{marca}','{modelo}', '{tags}');")
+cursor = mysql.cursor()
+cursor.execute(
+    buscador(id, marca, modelo, precio, tags)
+)
+
+mysql.commit()
---
+++ test.py
@@ -0,0 +1,4 @@
+secundario = int(input("Ahora de estas 3 marcas selecciona la que mas te guste: "))
+segunda_seleccion = {1: "elegante", 2:"señora", 3:"joven"}
+
+print(segunda_seleccion[secundario])
